11b33df5af450ffc7e3f62aa2497a7d7a93bc71b
AndrewMohawk <andrew@andrewmohawk.com>
2021-03-29 22:50:48 -0700

    Updating install and update scripts and setting default config file

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+echo -e "
+ ▄▄▄       █    ██  ██▀███   ▒█████   ██▀███   ▄▄▄      
+▒████▄     ██  ▓██▒▓██ ▒ ██▒▒██▒  ██▒▓██ ▒ ██▒▒████▄    
+▒██  ▀█▄  ▓██  ▒██░▓██ ░▄█ ▒▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  
+░██▄▄▄▄██ ▓▓█  ░██░▒██▀▀█▄  ▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██ 
+ ▓█   ▓██▒▒▒█████▓ ░██▓ ▒██▒░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒
+ ▒▒   ▓▒█░░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░
+  ▒   ▒▒ ░░░▒░ ░ ░   ░▒ ░ ▒░  ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░
+  ░   ▒    ░░░ ░ ░   ░░   ░ ░ ░ ░ ▒    ░░   ░   ░   ▒   
+      ░  ░   ░        ░         ░ ░     ░           ░  ░
+                -Install Script-
+                @AndrewMohawk                                                        
+"
+INSTALLDIR="/opt/Aurora"
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root, please use sudo" 
+   exit 1
+fi
+
+if [ -d "$INSTALLDIR" ] 
+then
+    echo "Directory $INSTALLDIR exists. Please remove before performing a clean install" 
+    exit 1
+fi
+echo "[+] Creating Install Directory"
+echo "------------------------------"
+mkdir $INSTALLDIR
+cd $INSTALLDIR
+echo "[+] Updating APT and installing dependencies"
+echo "------------------------------"
+apt update
+apt-get install -y libatlas-base-dev libportaudio2 python3-pip git python3-opencv
+echo "[+] Git Cloning Aurora base"
+echo "------------------------------"
+git clone https://github.com/AndrewMohawk/Aurora.git .
+cp config.ini.bak config.ini
+echo "[+] Installing Python Requirements"
+echo "------------------------------"
+pip3 install -r requirements.txt
+echo "[+] Installing Service"
+echo "------------------------------"
+cp aurora.service /etc/systemd/system
+systemctl start aurora.service
+echo "[+] Service Status"
+echo "------------------------------"
+echo "Aurora status: `systemctl is-active aurora.service`"
+echo "[+] Last 20 lines of syslog"
+echo "------------------------------"
+sudo tail -n 20 /var/log/syslog
+echo -e "
+ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ 
+(___|___|___|___|___|___|___|___|___|___|___|___)
+        "
+echo "Complete. You can now browse to the web interface to configure any changes you may need."
+localip=$(hostname -I)
+echo "This will likely be: http://$localip/"
---
+++ update.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo -e "
+ ▄▄▄       █    ██  ██▀███   ▒█████   ██▀███   ▄▄▄      
+▒████▄     ██  ▓██▒▓██ ▒ ██▒▒██▒  ██▒▓██ ▒ ██▒▒████▄    
+▒██  ▀█▄  ▓██  ▒██░▓██ ░▄█ ▒▒██░  ██▒▓██ ░▄█ ▒▒██  ▀█▄  
+░██▄▄▄▄██ ▓▓█  ░██░▒██▀▀█▄  ▒██   ██░▒██▀▀█▄  ░██▄▄▄▄██ 
+ ▓█   ▓██▒▒▒█████▓ ░██▓ ▒██▒░ ████▓▒░░██▓ ▒██▒ ▓█   ▓██▒
+ ▒▒   ▓▒█░░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░
+  ▒   ▒▒ ░░░▒░ ░ ░   ░▒ ░ ▒░  ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░
+  ░   ▒    ░░░ ░ ░   ░░   ░ ░ ░ ░ ▒    ░░   ░   ░   ▒   
+      ░  ░   ░        ░         ░ ░     ░           ░  ░
+                -Update Script-
+                @AndrewMohawk                                                        
+"
+INSTALLDIR = "/opt/Aurora"
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root, please use sudo" 
+   exit 1
+fi
+
+if [ !-d "$INSTALLDIR" ] 
+then
+    echo "Directory $INSTALLDIR Does not exist. Please run install first or update the install directory" 
+    exit 1
+fi
+cd $INSTALLDIR
+if [ -d .git ]; then
+    echo "[+] Git Cloning Aurora base"
+    echo "------------------------------"
+    git pull 
+    echo "[+] Stopping Aurora"
+    echo "------------------------------"
+    service aurora stop 
+    echo "[+] Service Status"
+    echo "------------------------------"
+    echo "Aurora status: `systemctl is-active aurora.service`"
+    echo "[+] Starting Aurora"
+    echo "------------------------------"
+    service aurora start 
+    echo "[+] Service Status"
+    echo "------------------------------"
+    echo "Aurora status: `systemctl is-active aurora.service`"
+    echo "[+] Last 20 lines of syslog"
+    echo "------------------------------"
+    sudo tail -n 20 /var/log/syslog
+    echo -e "
+ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ ___ 
+(___|___|___|___|___|___|___|___|___|___|___|___)
+        "
+    echo "Complete. You can now browse to the web interface to configure any changes you may need."
+    localip=$(hostname -I)
+    echo "This will likely be: http://$localip/"
+
+else
+    echo "This install directory ($INSTALLDIR) is *NOT* a git repo."
+fi;
